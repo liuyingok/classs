8ad443a2fc32c4792570253a71059cf8dcdf9981
ly <li@qq.com>
2017-06-04 23:53:23 +0800

    第七章结束

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,36 +1,64 @@
 <template>
-  <div class="shopcart">
-    <div class="content">
-      <div class="content-left">
-        <div class="logo-wrapper">
-          <div class="logo" :class="{'highlight':totalCount>0}">
-            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+  <div>
+    <div class="shopcart">
+      <div class="content"  @click="toggleList">
+        <div class="content-left">
+          <div class="logo-wrapper">
+            <div class="logo" :class="{'highlight':totalCount>0}">
+              <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+            </div>
+            <div class="num"  v-show="totalCount>0">{{totalCount}}</div>
           </div>
-          <div class="num"  v-show="totalCount>0">{{totalCount}}</div>
+          <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}</div>
+          <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
         </div>
-        <div class="price" :class="{'highlight':totalPrice>0}">￥{{totalPrice}}</div>
-        <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+        <div class="content-right"  @click.stop.prevent="pay">
+          <div class="pay" :class="payClass">
+            {{payDesc}}
+          </div>
+        </div>
       </div>
-      <div class="content-right">
-        <div class="pay" :class="payClass">
-          {{payDesc}}
+      <div class="ball-container">
+        <transition-group name="drop"
+                          v-on:before-enter="beforeEnter"
+                          v-on:enter="enter"
+                          v-on:after-enter="afterEnter">
+          <div  v-for="ball in balls"  key="a"  v-show="ball.show" class="ball"  >
+            <div class="inner inner-hook"  key="b"></div>
+          </div>
+        </transition-group>
+      </div>
+      <transition name="fold">
+        <div class="shopcart-list" v-show="listShow">
+        <div class="list-header">
+          <h1 class="title">购物车</h1>
+          <span class="empty"  @click="empty">清空</span>
+        </div>
+        <div class="list-content"   ref="listContent">
+          <ul>
+            <li class="food" v-for="food in selectFoods">
+              <span class="name">{{food.name}}</span>
+              <div class="price">
+                <span>￥{{food.price*food.count}}</span>
+              </div>
+              <div class="cartcontrol-wrapper">
+                <cartcontrol :food="food"></cartcontrol>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
+      </transition>
     </div>
-    <div class="ball-container">
-      <transition-group name="drop"
-                        v-on:before-enter="beforeEnter"
-                        v-on:enter="enter"
-                        v-on:after-enter="afterEnter">
-        <div  v-for="ball in balls"  key="a"  v-show="ball.show" class="ball"  >
-          <div class="inner inner-hook"  key="b"></div>
-        </div>
-      </transition-group>
-    </div>
+    <!--背景遮罩层-->
+    <transition name="fade">
+      <div class="list-mask" @click="hideList" v-show="listShow"></div>
+    </transition>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import cartcontrol from '../../components/cartcontrol/cartcontrol.vue';
+  import BScroll from 'better-scroll';
   export default{
     props: {
       selectFoods: {
@@ -67,10 +95,31 @@
             show: false
           }
         ],
-        dropBalls: []
+        dropBalls: [],
+        fold: true // 购物车详情页折叠状态,true表示折叠
       };
     },
     methods: {
+      toggleList() {
+        if (!this.totalCount) {
+          return;
+        }
+        this.fold = !this.fold;
+      },
+      empty() {
+        this.selectFoods.forEach((food) => {
+          food.count = 0;
+        });
+      },
+      pay() {
+        if (this.totalPrice < this.minPrice) {
+          return;
+        }
+        window.alert(`支付${this.totalPrice}元`); //
+      },
+      hideList() {
+        this.fold = true;
+      },
       drop (el) {
         for (var i = 0; i < this.balls.length; i++) {
           let ball = this.balls[i];
@@ -148,6 +197,25 @@
         } else {
           return 'enough';
         }
+      },
+      listShow() {
+        if (!this.totalCount) {
+          this.fold = true;
+          return false;
+        }
+        let show = !this.fold;
+        if (show) {
+          this.$nextTick(() => {
+            if (!this.scroll) { // 如果没有better-scroll的实例则添加
+              this.scroll = new BScroll(this.$refs.listContent, {
+                click: true
+              });
+            } else {
+              this.scroll.refresh(); // refresh计算是否需要滚动
+            }
+          });
+        }
+        return show;
       }
     },
     components: {
@@ -156,110 +224,5 @@
   };
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-  .shopcart
-    position: fixed
-    left: 0
-    bottom: 0
-    z-index: 50
-    width: 100%
-    height: 48px
-    .content
-      display: -webkit-box
-      display: -ms-flexbox
-      display: flex
-      background: #141d27
-      font-size: 0
-      color: rgba(255,255,255,0.4)
-      .content-left
-        -webkit-box-flex: 1
-        -ms-flex: 1
-        flex: 1
-        .logo-wrapper
-          display: inline-block
-          vertical-align: top
-          position: relative
-          top: -10px
-          margin: 0 12px
-          padding: 6px
-          width: 56px
-          height: 56px
-          box-sizing: border-box
-          border-radius: 50%
-          background: #141d27
-          .logo
-            width: 100%
-            height: 100%
-            border-radius: 50%
-            text-align: center
-            background: #2b343c
-            &.highlight
-              background: #00a0dc
-            .icon-shopping_cart
-              line-height: 44px
-              font-size: 24px
-              color: #80858a
-              &.highlight
-                color: #fff
-          .num
-            position: absolute
-            top: 0
-            right: 0
-            width: 24px
-            height: 16px
-            line-height: 16px
-            text-align: center
-            border-radius: 16px
-            font-size: 9px
-            font-weight: 700
-            color: #fff
-            background: #f01414
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4)
-        .price
-          display: inline-block
-          vertical-align: top
-          margin-top: 12px
-          line-height: 24px
-          padding-right: 12px
-          box-sizing: border-box
-          border-right: 1px solid rgba(255,255,255,0.1)
-          font-size: 16px
-          font-weight: 700
-          &.highlight
-            color: #fff
-        .desc
-          display: inline-block
-          vertical-align: top
-          margin: 12px 0 0 12px
-          line-height: 24px
-          font-size: 10px
-      .content-right
-        -webkit-box-flex: 0
-        -ms-flex: 0 0 105px
-        flex: 0 0 105px
-        width: 105px
-        .pay
-          height: 48px
-          line-height: 48px
-          text-align: center
-          font-size: 12px
-          font-weight: 700
-          &.not-enough
-            background: #2b333b
-          &.enough
-            background: #00b43c
-            color: #fff
-    .ball-container
-      .ball
-        bottom: 22px
-        left: 32px
-        position: fixed
-        z-index: 200
-        &.drop-enter-active, &.drop-leave-active
-          transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41) 0s;
-          .inner
-            background: #00a0dc none repeat scroll 0 0
-            border-radius: 50%
-            height: 16px
-            transition: all 0.4s linear 0s
-            width: 16px
+  @import "./shopcart.styl";
 </style>
