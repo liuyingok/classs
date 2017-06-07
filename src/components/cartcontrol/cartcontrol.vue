ae333e25a49977aa2cda30ce6688a8a9a7b0ee9c
liuyingok <247220246@qq.com>
2017-06-07 13:33:54 +0800

    试试有没有有问题

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="drop">
+      <div @click="decreaseCart" class="cart-decrease move-transition" v-show="food.count>0">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add inner-hook icon-add_circle" @click="addCart"></div>
+
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import Vue from 'vue';
+  export default{
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart (event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+        this.$emit('event', event.target);
+      },
+      decreaseCart (event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (this.food.count) {
+          this.food.count--;
+        }
+      }
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./cartcontrol.styl";
+</style>
